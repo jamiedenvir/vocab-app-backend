a4a6703a04a3254b7bf1949f1285203d569e1e80
jamie denvir <jwdenvir@gmail.com>
2021-06-20 16:26:47 -0400

    creates associations between models, implements rails api authentication guide for users, and creates dummy data to test associations

--- app/models/example.rb
+++ app/models/example.rb
@@ -1,2 +1,21 @@
 class Example < ApplicationRecord
+  belongs_to :user
+  belongs_to :prompt 
+
+  response.headers["app_id"] = "07e4e85c",["app_key"] = "2434c7ca68bd567abc4fee242c3c9381"
+  
+  def word
+
+
+    
+    
+    response = HTTP.get("https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/absolve?strictMatch=false")
+    
+    
+    response.parse(:json)
+
+    
+
+
+  end
 end
--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,2 +1,3 @@
 class Prompt < ApplicationRecord
+  has_many :examples
 end
