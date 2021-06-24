059029506bc2cc2b42e7aee43f6ffab45c4603b5
jamie denvir <jwdenvir@gmail.com>
2021-06-24 11:56:08 -0400

    adds api word data object

--- app/models/example.rb
+++ app/models/example.rb
@@ -8,7 +8,33 @@ class Example < ApplicationRecord
   
     response = HTTP.headers("app_id" => Rails.application.credentials.app_id, "app_key" => Rails.application.credentials.app_key).get("https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/#{word}?strictMatch=false")
  
-    response.parse(:json)
+    api_word_data_object = response.parse(:json)["results"][0]
+
+    return {
+      id: api_word_data_object["id"],
+
+      etymologies: api_word_data_object["lexicalEntries"][0]["entries"][0]["etymologies"],
+
+      grammatical_features: api_word_data_object["lexicalEntries"][0]["entries"][0]["grammaticalFeatures"][0]["id"],
+      
+      audio: api_word_data_object["lexicalEntries"][0]["entries"][0]["pronunciations"][0]["audioFile"],
+
+      phonetic_spelling: api_word_data_object["lexicalEntries"][0]["entries"][0]["pronunciations"][0]["phoneticSpelling"],
+
+      definition: api_word_data_object["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"],
+
+      example_sentence: api_word_data_object["lexicalEntries"][0]["entries"][0]["senses"][0]["examples"][0]["text"],
+
+      synonyms: [ 
+        api_word_data_object["lexicalEntries"][0]["entries"][0]["senses"][0]["synonyms"][0]["text"],
+        api_word_data_object["lexicalEntries"][0]["entries"][0]["senses"][0]["synonyms"][1]["text"],
+        api_word_data_object["lexicalEntries"][0]["entries"][0]["senses"][0]["synonyms"][2]["text"],
+        api_word_data_object["lexicalEntries"][0]["entries"][0]["senses"][0]["synonyms"][3]["text"],
+        api_word_data_object["lexicalEntries"][0]["entries"][0]["senses"][0]["synonyms"][4]["text"],
+      ]
+
+      
+    }
 
   end
 end
