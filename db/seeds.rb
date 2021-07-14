06f0ebf76e8d2406757718aa032048a3be25820d
jamie denvir <jwdenvir@gmail.com>
2021-07-14 13:30:39 -0400

    seeds prompt data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Prompt.create! ([
+  
+  # {text: "If you think that's weird, listen to THIS:"},
+  # {text: "I will not be going with you. Here's why:"},
+  # {text: "WHAT?!"},
+  # {text: "My big toe has never been more sore. Here's what happened:"},
+  # {text: "Take another step. I dare you. I'll..."},
+  # {text: "Nobody knows the trouble I've seen."},
+  # {text: "I'm as happy as a damn clam!"}
+  {image_url: "https://media.istockphoto.com/photos/funny-man-with-watermelon-helmet-and-goggles-picture-id187722063?k=6&m=187722063&s=612x612&w=0&h=5Zz-oL69FsM3vROIAA8n9btXR0UvrKZZPbSDlOQbZbM="},
+  {image_url: "http://www.kittyworks.com/images/tfxn4hoc6hi-i.jpg"},
+  {image_url: "https://lh3.googleusercontent.com/proxy/9GPZaitXtGdIxQJF8PLl1ki8ra2k-wn4n8ZL_NGeP9A0elFC593mph1j6miMFCtWdW4uoodvoDODy7f19NOUJ-oxX9MDGVPveTT332cGz0lkH8YT6ms"},
+  {image_url: "https://images.pexels.com/photos/1767434/pexels-photo-1767434.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"},
+  {image_url: "https://media.istockphoto.com/photos/adorable-welsh-corgi-pembroke-on-green-lawn-at-home-picture-id1077145200?k=6&m=1077145200&s=612x612&w=0&h=y_N-ix0MCWZJENTIDnL7DWMzJzlASHaE5bqCjDZyiNM="},
+  {image_url: "https://qph.fs.quoracdn.net/main-qimg-1b1cf936bb3cd7eb441b79ad2434eb08.webp"},
+  {image_url: "https://www.jellycat.com/images/products/large/SIL3N.jpg"},
+  {image_url: "https://www.jantecneon.com/mm5/graphics/00000001/HS-25-47.png"},
+  {image_url: "https://images-na.ssl-images-amazon.com/images/I/71P6Qaye4KL._AC_SL1137_.jpg"},
+  {image_url: "https://i.ytimg.com/vi/mvzxIV6cWNU/maxresdefault.jpg"},
+  {image_url: "https://i.pinimg.com/736x/dd/ac/47/ddac471a3bea477e5e11bd94ced3f854.jpg"}
+])
